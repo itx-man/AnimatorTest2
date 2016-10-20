b7330943ca03f9a8990af2b9891ba7801f499324
芮元乐 <ruiyuanle@bbktel.com>
2016-10-20 17:42:10 +0800

    Initial commit

---
+++ app/src/main/java/com/vivi/animatortest2/MainActivity.java
@@ -0,0 +1,64 @@
+package com.vivi.animatortest2;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                ObjectAnimator animator = ObjectAnimator.ofFloat(v, "alpha", 0F, 1F);
+                animator.setDuration(1000);
+                //给特定的事件添加监听，不需重写所有的方法
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        Toast.makeText(MainActivity.this, "anim end", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //给动画添加监听事件，方法1
+                /*animator.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Toast.makeText(MainActivity.this, "anim end", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });*/
+                animator.start();
+                break;
+        }
+    }
+}
